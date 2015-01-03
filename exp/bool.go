7b60a05ed7c6c94fe42ccfb383aa0fbbaf699772
alexkappa <alex.kalyvitis@gmail.com>
2015-01-03 11:00:47 +0100

    commend on Bool

--- exp/bool.go
+++ exp/bool.go
@@ -2,8 +2,11 @@ package exp
 
 // Boolean
 
+// Bool is a wrapper for the native bool type which provides an Eval function so
+// that it satisfies the Exp interface.
 type Bool bool
 
+// Eval will always return the boolean value of b and disregard p.
 func (b Bool) Eval(p Params) bool {
 	return bool(b)
 }
